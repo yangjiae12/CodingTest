f8c6de46bac2b2fb4ff820caff7debc1154d807b
yangjiae12 <140368421+yangjiae12@users.noreply.github.com>
2024-12-31 23:10:12 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,18 @@
+// 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
+
+// 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cin >> year;
+    
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        cout << 1;
+    else
+        cout << 0;
+    
+    return 0;
+}
