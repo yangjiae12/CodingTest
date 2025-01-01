826b8d34190be1b4edd88793bb692d8b143b422a
yangjiae12 <140368421+yangjiae12@users.noreply.github.com>
2025-01-01 13:08:14 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -4,30 +4,25 @@ using namespace std;
 int main() {
     int n;  // 테스트 케이스 개수
     cin >> n;
-    
-    for (int i = 0; i < n; i++) {  // 각 테스트 케이스
+
+    for (int t = 0; t < n; t++) {
         string results;
         cin >> results;
-        
-        int score = 0;  // 각 테스트 케이스의 점수
-        
+
+        int score = 0;  // 점수
+        int current_streak = 0;  // 연속된 'O'의 개수
+
         for (int i = 0; i < results.size(); i++) {
             if (results[i] == 'O') {
-                int current_streak = 0;  // 연속된 0의 개수
-                for (int j = i; j >= 0; j--) {
-                    if (results[j] == 'O') {
-                        current_streak++;
-                    }
-                    else {
-                        break;
-                    }
-                }
-                score += current_streak;
+                current_streak++;
+                score += current_streak;  // 연속 점수 누적
+            } else {
+                current_streak = 0;  // 연속 끊김
             }
         }
-        
-        cout << score << endl;
-    } 
-    
+
+        cout << score << endl;  // 결과 출력
+    }
+
     return 0;
 }
