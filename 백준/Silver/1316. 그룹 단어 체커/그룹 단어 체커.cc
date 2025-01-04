c8ac3866e7d55fa529faab26e93f4d5e526481d1
yangjiae12 <140368421+yangjiae12@users.noreply.github.com>
2025-01-04 14:54:50 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -4,38 +4,29 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
-    int pcs = 0;
-    
+
+    int pcs = 0;  // 그룹 단어 개수
+
     for (int i = 0; i < n; i++) {
         string group;
         cin >> group;
-        
-        int check[26] = {0};
-        char prev;
+
+        int check[26] = {0};  // 알파벳 체크 배열
         bool isGroupWord = true;
-        
+
         for (int j = 0; j < group.length(); j++) {
-            check[group[j] - 'a']++;
-            prev = group[j - 1];
-            if (j == 0) {
-                isGroupWord = true;
-            }
-            else if (check[group[j] - 'a'] == 1) {
-                isGroupWord = true;
-            }
-            else if (check[group[j] - 'a'] > 0 && prev != group[j]) {
+            // 문자가 이미 등장했으면서, 이전 문자와 다르면 그룹 단어가 아님
+            if (check[group[j] - 'a'] > 0 && group[j - 1] != group[j]) {
                 isGroupWord = false;
                 break;
             }
+
+            check[group[j] - 'a']++;  // 문자 체크
         }
-        
-        if (isGroupWord) {
-            pcs++;
-        }
+
+        if (isGroupWord) pcs++;  // 그룹 단어일 경우 개수 증가
     }
-    
+
     cout << pcs << endl;
-    
     return 0;
 }
